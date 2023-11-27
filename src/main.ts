b921c4dee8530459b7c42eb77f77c889c6fc4130
Leider <leideratorres@unicesar.edu.co>
2023-11-26 20:59:43 -0500

    Cors

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,7 @@ import * as cors from 'cors';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const express = require('express');
-  const cors = require('cors');
 
-  const corsOptions = {
-    origin: 'http://localhost:4200',
-    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-  };
-
-  app.use(cors(corsOptions));
 
   const options = new DocumentBuilder()
     .setTitle('Ventas REST API')
@@ -24,6 +17,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
 
   SwaggerModule.setup('docs', app, document);
+  app.enableCors();
   await app.listen(4001);
 }
 bootstrap();
